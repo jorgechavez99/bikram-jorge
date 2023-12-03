ea243c96500a36c9c289dde67a80d1c4ca43a8c3
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2023-12-03 21:50:36 +0100

    archivojs faltabucles

---
+++ index.js
@@ -0,0 +1,31 @@
+let variableSinValor=undefined;
+let booleano1=true;
+let booleano2=false;
+const PI=3.14;
+const TAU= 2*PI;
+let booleanoAnd= booleano1 && booleano2;
+let booleanoNot= booleano1 && booleano2;
+let booleanoMix0= ((booleano1||booleano2) && (booleano1||(!booleano1&&!booleano2)));
+console .log(typeof booleanoMix0);
+console .log(booleanoMix0);
+let incrementarDesp=2+1;
+let resultadoDesp=incrementarDesp-1;
+let incrementarAntes=2+1;
+let resultadoAntes=incrementarAntes;
+let contarHasta10_2=10;
+// Faltaria bucles 11,12
+let variableValorNumerico=2;
+const MiNombre="tu nombre";
+console .log(typeof Minombre);
+const MiNumeroFav=7;
+let booleanoOr=booleano1 || booleano2;
+let booleanoMix1= (booleano1 && (TAU/2 == PI))||(variableValorNumerico>=MiNumeroFav);
+let seisNoEsNueve=6!=9;
+let booleanoMix2=(variableValorNumerico)<(MiNumeroFav*TAU);
+console .log( typeof booleanoMix2)
+let valorSuma=MiNumeroFav + variableValorNumerico;
+let valorResta=MiNumeroFav - variableValorNumerico;
+let valorMultiplicacion=MiNumeroFav * variableValorNumerico;
+let valorDivision=MiNumeroFav / 3;
+let contarHasta10=10;
+
